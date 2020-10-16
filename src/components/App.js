92723aa6daec5555fb5e4e9c3d0593c1932e4a92
tbednarz <tresbednarz@gmail.com>
2020-10-15 23:10:13 -0400

    styles

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,8 @@ import "../App.css";
 function App() {
   return (
     <div className="Home-Tab">
-      <img
-        id="tres-pic"
-        alt="My photo"
-        src="../tres.jpg"
-        width="300px"
-        height="450px"
-      />
-
+      
+   
       <div className="Home-Text-Div">
         {" "}
         Hi, My name is Trés! A Welder and Heavy Diesel Mechanic turned
