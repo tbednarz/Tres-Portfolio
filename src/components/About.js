f2e65ec6f9a0bbad8b72ccbe3561292243b6172c
Tres Bednarz <tresbednarz@gmail.com>
2021-06-20 01:38:37 -0400

    about me

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,16 @@ const About = () => {
       <div id="about-left">
         <h1>About Me</h1>
         <p id="about-me-p">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure dolore
-          optio, fugiat dolor qui, nesciunt beatae consequuntur repellendus
-          atque et iste amet blanditiis quod? Totam ab iusto enim ipsa qui
-          veritatis a voluptates tempore eius porro voluptatem earum vel dicta,
-          maiores saepe ut quod quis possimus modi molestiae eum repellendus?
+          Hi I am a former Welder/Fabricator/Mechanic/Salesperson turned
+          Software Developer! I began my journey by making a simple program to
+          help my mother with her job and was hooked into programming from then
+          on. I have spent the last 4 years teaching myself to build websites
+          and utilities using JavaScript and Python. When I am not programming I
+          will usually be reading Harry Potter for the 19th time,
+          drawing/painting, biking or wandering around taking pictures of things
+          I find interesting. I am looking for a position working with a team
+          who embraces each other learning new things while building interesting
+          and impactful projects.
         </p>
       </div>
       <div id="about-right"></div>
