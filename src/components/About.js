92165d29505320a7a28b06f176e024eaa5b240b3
Tres <tresbednarz@gmail.com>
2022-02-08 13:40:13 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 <p id="about-me-p">
                     Hi, I am a former Welder/Fabricator/Mechanic/ turned
                     Software Developer! I have spent the last 4 years teaching
-                    myself to build websites and utilities using JavaScript and
+                    myself to build websites and utilities using HTML/CSS, JavaScript, React and
                     Python. I am looking for a position working with a team who
                     embraces each other learning new things while building
                     interesting and impactful projects.
